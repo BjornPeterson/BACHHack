a69f409488c12bd5d8beafaa51d279eec95455bf
Michael <michael@rehive.com>
2018-10-10 21:04:27 -0700

    bch integrations scritps
    address monitoring files

---
+++ sending.js
@@ -0,0 +1,3 @@
+//Authentication
+var blocktrail = require('blocktrail-sdk');
+client = blocktrail.BlocktrailSDK({apiKey: "3265829528aba9920420c411a67cdf8fe65a7dc4", apiSecret: "9414db25da6f25eddb2db69b5ea38fc0709b9503", network: "tBCH", testnet: true});
