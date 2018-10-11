946f8f19f8edf887f65946efdf5996ec5c232f1e
Michael <michael@rehive.com>
2018-10-10 23:21:42 -0700

    added blockchain send function
    testting blockchain send function

--- app.js
+++ app.js
@@ -13,16 +13,29 @@ app.get('/', (req, res) => {
 });
 // [END hello_world]
 
-//2NGZRVn1btCG4Xxd9esSnFXiHAP6DrBL4yW :: MERCHANT (temp) ////  / prlmhl94mpr74yuang0azmkyj8xknwr9gg7rtl58dj
-   //2MuFZLfZLMTPDqzRRkU4va36EwqW1VubAnC :: CONSUMER (michaelTest) //// pqtqqe7ztzxfj8yv7ys8m6464xd9755cv5nccg8e6z / prypsl76p0n2rzut7f59p8akmy3eqkn7qs954a3vy8 / pqprlhsyhp6uq0ap448q0tdu372nry48v587q28chr
+   //Demo wallet balances
+    //2NGZRVn1btCG4Xxd9esSnFXiHAP6DrBL4yW :: MERCHANT (temp) ////  / prlmhl94mpr74yuang0azmkyj8xknwr9gg7rtl58dj
+    //2MuFZLfZLMTPDqzRRkU4va36EwqW1VubAnC :: CONSUMER (michaelTest) //// pqtqqe7ztzxfj8yv7ys8m6464xd9755cv5nccg8e6z / prypsl76p0n2rzut7f59p8akmy3eqkn7qs954a3vy8 / pqprlhsyhp6uq0ap448q0tdu372nry48v587q28chr
+    //2NFWQDTX7KixEmgQm2DZvBVtZ4UrZdrGzQU :: COMMUNITY (MiketBTC) //// pr6rrze6y6h5gfu9lh4s3d5n22h59jau8va5vv9p0c 
+
+
 function Pay(C_ad, amount) {
   
+//Authentication
+var blocktrail = require('blocktrail-sdk');
+client = blocktrail.BlocktrailSDK({apiKey: "3265829528aba9920420c411a67cdf8fe65a7dc4", apiSecret: "9414db25da6f25eddb2db69b5ea38fc0709b9503", network: "tBCH", testnet: true});
   
-  //michael logic
-           
+  client.initWallet("MiketBTC", "Br!nk123", function(err, wallet) { 
+    
+    //Initiate transaction
+        value = blocktrail.toSatoshi(amount);
+        wallet.pay({C_ad : value}, //sending to CONSUMER wallet
+            function(err, result) {
+                //console.log(result);
+            });
+    });
 
 }
-
 
  // POST method route
 app.post('/', function (req, res) {
@@ -37,7 +50,7 @@ app.post('/', function (req, res) {
 
 res.json(req.body)
 
-Pay('2MuFZLfZLMTPDqzRRkU4va36EwqW1VubAnC',100000)
+Pay('2MuFZLfZLMTPDqzRRkU4va36EwqW1VubAnC',0.00777)
 
  // var myObj = JSON.parse(req.body);
    //var myObj = req.network;
